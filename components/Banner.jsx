ad11437435fed4c8e8e4133983dbb423fa6f81af
Andres Espejo <jeaespejogo@unal.edu.co>
2020-12-29 21:19:45 -0500

    add banner

---
+++ components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Banner = () => {
+  return (
+    <div className='body'>
+      <div class='big-image'>
+        <div class='overlay'>
+          <h1>Hero Image With Overlay</h1>
+          <p>A full width background image built with HTML & CSS.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
